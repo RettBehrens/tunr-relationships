02052f6f91914c74cd9a0250d3ea512021969ce6
RettBehrens <everettbehrens@gmail.com>
2017-08-22 07:42:25 -0600

    stuck on sprint 2

--- starter-code/src/db/seed.ts
+++ starter-code/src/db/seed.ts
@@ -1,15 +1,22 @@
 import { db } from '../models';
 let DB = db.models;
 
-var artistCreate = function() {
-	return DB.Artist.create({
-    name: 'Luciano Pavarotti',
-    photoUrl: 'http://img.informador.com.mx/biblioteca/imagen/677x508/811/810055.jpg',
-    nationality: 'Italiano',
-    instrument: 'Voice',
-    home_address: '1 Strada Roma'
-  });
-};
+var lucySongs = [
+	{
+		title: "O sole mio",
+		duration: "3:21",
+		date_of_release: "1990",
+		album_title: "Three Tenors in Concert",
+		artistId: ""
+	},
+	{
+		title: "Nessun dorma",
+		duration: "3:21",
+		date_of_release: "1990",
+		album_title: "Three Tenors in Concert",
+		artistId: ""
+	}
+];
 
 var managerCreate = function() {
 	return DB.Manager.create({
@@ -17,7 +24,24 @@ var managerCreate = function() {
     email: 'rbobby@gmail.com',
     office_number: '516-877-0304',
     cell_phone_number: '718-989-1231'
-	});
+	})
+	.then(artistCreate());
+};
+
+var artistCreate = function() {
+	return DB.Artist.create({
+    name: 'Luciano Pavarotti',
+    photoUrl: 'http://img.informador.com.mx/biblioteca/imagen/677x508/811/810055.jpg',
+    nationality: 'Italiano',
+    instrument: 'Voice',
+    home_address: '1 Strada Roma'
+  })
+  .then(function(artist) {
+		lucySongs.forEach(function(song) {
+			song.artistId = artist.id;
+		});
+		DB.Song.bulkCreate(lucySongs);
+	})
 };
 
 var songCreate = function() {
@@ -29,8 +53,8 @@ var songCreate = function() {
 	});
 };
 
-artistCreate()
-.then(managerCreate)
+// artistCreate()
+managerCreate()
 .then(songCreate)
 .then(function() {
 	process.exit();
